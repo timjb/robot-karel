7624f8c49913d76f4b62c928f0185e045a6b7037
Tim Baumann <tim@timbaumann.info>
2011-02-21 23:11:00 +0100

    Add simple file server

---
+++ server.js
@@ -0,0 +1,13 @@
+var express = require('express')
+
+var app = express.createServer()
+
+app.configure(function() {
+  app.use(express.staticProvider(__dirname))
+})
+
+app.configure('production', function() {
+  app.use(express.errorHandler())
+})
+
+app.listen(8000)
