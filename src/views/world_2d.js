1e6b68e769cb35cdc2904d2663810ee4f97d5b08
Tim Baumann <tim@timbaumann.info>
2011-02-23 22:47:04 +0100

    Use <table> instead of <canvas> in views/world_2d

--- src/views/world_2d.js
+++ src/views/world_2d.js
@@ -13,10 +13,32 @@ module.exports = require('views/world_base').extend({
       .bind('dom:insert', this.delegateEvents)
       .bind('dom:insert', this.render)
     
-    $(window).resize(this.render)
+    this.createTable()
   },
 
-  tagName: 'canvas',
+  createTable: function() {
+    var width     = this.model.get('width')
+    ,   height    = this.model.get('depth')
+    
+    this.fields = []
+    
+    var el = $(this.el)
+    for (var i = 0; i < height; i++) {
+      var tr = $('<tr />')
+      this.fields[i] = []
+      for (var j = 0; j < width; j++) {
+        var td = $('<td />')
+        td.attr('data-x', j)
+        td.attr('data-y', i)
+        this.fields[i][j] = td
+        tr.append(td)
+      }
+      el.append(tr)
+    }
+  },
+
+  tagName: 'table',
+  className: 'world-2d',
 
   events: {
     mousedown:   'onMousedown',
@@ -27,13 +49,12 @@ module.exports = require('views/world_base').extend({
   },
 
   onMousedown: function(evt) {
-    var rect = $(this.el).offset()
-    ,   x = evt.clientX - rect.left
-    ,   y = evt.clientY - rect.top
+    var td = $(evt.target)
+    if (!td.is('td')) return
     
     var position = new Position(
-      Math.floor((x-this.GAP) / this.GW),
-      Math.floor((y-this.GAP) / this.GW)
+      parseInt(td.attr('data-x'), 10),
+      parseInt(td.attr('data-y'), 10)
     )
     
     if (evt.which == 1) { // left click
@@ -48,57 +69,36 @@ module.exports = require('views/world_base').extend({
   render: function() {
     console.log('Render 2D')
     
-    var parent = $(this.el).parent()
-    ,   width  = parent.innerWidth()
-    ,   height = parent.innerHeight()
+    var el = $(this.el)
+    el.css('display', 'none')
     
-    var model     = this.model
-    ,   robot     = model.get('robot')
-    ,   position  = robot.get('position')
-    ,   direction = robot.get('direction')
-    
-    var GAP = this.GAP = 4
-    ,   GW  = this.GW  = Math.round(Math.min(
-      (width-GAP)  / model.get('width'),
-      (height-GAP) / model.get('depth')
-    )) // GridWidth
-    
-    $(this.el).attr({ width: width, height: height })
-    var ctx = this.el.getContext('2d')
-    
-    // Fill everything => border between squares
-    ctx.fillStyle = '#333'
-    ctx.fillRect(0, 0, width, height)
-    
-    function fill(x, y, color) {
-      ctx.fillStyle = color
-      ctx.fillRect(GAP+x*GW, GAP+y*GW, GW-GAP, GW-GAP)
-    }
-    
-    function letter(x, y, color, letter) {
-      ctx.fillStyle = color
-      ctx.textAlign = 'center'
-      ctx.textBaseline = 'middle'
-      ctx.font = (0.5*GW) + 'px Helvetica, Arial, sans-serif'
-      ctx.fillText(letter, GAP + x*GW + 0.5*(GW-GAP), GAP + y*GW + 0.5*(GW-GAP))
-    }
+    var model = this.model
+    var fields = this.fields
     
     model.eachField(function(x, y, field) {
-      var bg, fg;
-      if      (field.quader) { bg = ENVIRONMENT_COLORS.quader.css }
-      else if (field.marke)  { bg = ENVIRONMENT_COLORS.marke.css;  fg = '#000' }
-      else if (field.ziegel) { bg = ENVIRONMENT_COLORS.ziegel.css; fg = '#fff' }
-      else    /* nothing */  { bg = '#fff'; fg = '#000' }
-      fill(x, y, bg)
-      if (position.x == x && position.y == y) {
-        var char;
-        if      (direction.isNorth()) char = '\u25b2'
-        else if (direction.isSouth()) char = '\u25bc'
-        else if (direction.isWest())  char = '\u25c4'
-        else    /* east */            char = '\u25ba'
-        letter(x, y, fg, char)
-      } else if (field.ziegel) letter(x, y, fg, field.ziegel)
-    });
+      var td = fields[y][x]
+      td[field.marke  ? 'addClass' : 'removeClass']('marke')
+      td[field.quader ? 'addClass' : 'removeClass']('quader')
+      td[field.ziegel ? 'addClass' : 'removeClass']('ziegel')
+      td.text(field.ziegel ? field.ziegel : '')
+    })
+    
+    ;(function() {
+      var robot        = model.get('robot')
+      ,   position     = robot.get('position')
+      ,   direction    = robot.get('direction')
+      ,   currentField = model.getField(position)
+      ,   currentTd    = fields[position.y][position.x]
+      
+      var char
+      if      (direction.isNorth()) char = '\u25b2'
+      else if (direction.isSouth()) char = '\u25bc'
+      else if (direction.isWest())  char = '\u25c4'
+      else    /* east */            char = '\u25ba'
+      currentTd.html(char)
+    })()
+    
+    el.css('display', '')
   }
 
 }, {
