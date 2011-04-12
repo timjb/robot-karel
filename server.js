9926f9cf27b2626b70d461334630c3884fc9aa50
Tim Baumann <tim@timbaumann.info>
2011-04-13 00:27:01 +0200

    [server] Getting started with express views

--- server.js
+++ server.js
@@ -7,15 +7,23 @@ var app = express.createServer()
 
 app.configure(function() {
   app.use(express.static(__dirname))
+  
   app.use(browserify({
     base:  __dirname + '/lib',
     mount: '/browserify.js',
     require: ['underscore', 'backbone']
   }))
+  
+  app.set('views', __dirname + '/views')
+  app.set('view engine', 'ejs')
 })
 
 app.configure('production', function() {
   app.use(express.errorHandler())
+})
+
+app.get('/', function (req, res) {
+  res.render('index')
 })
 
 app.listen(8000)
