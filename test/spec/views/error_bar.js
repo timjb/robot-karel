25b58cbbef56ce17ac246c32af33cf60c8ca787b
Tim Baumann <tim@timbaumann.info>
2011-05-14 00:19:24 +0200

    Tests for ErrorBar

---
+++ test/spec/views/error_bar.js
@@ -0,0 +1,23 @@
+describe("Error Bar", function() {
+  var project, errorBar
+
+  beforeEach(function() {
+    project = new Karel.Models.Project({ code: 'asdf' })
+    errorBar = new Karel.Views.ErrorBar({ model: project })
+    errorBar.render()
+  })
+
+  it("should show the bar", function() {
+    var el = $(errorBar.el)
+    expect(el.html()).toEqual('')
+    project.run()
+    expect(el.html()).toMatch(/asdf .* not defined/)
+  })
+
+  it("should hide the bar", function() {
+    project.run()
+    errorBar.$('a').click()
+    expect($(errorBar.el).html()).toEqual('')
+  })
+
+})
