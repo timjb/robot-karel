332b2c3e7d199ca9d59814447f03a16c8f173226
Tim Baumann <tim@timbaumann.info>
2011-08-12 23:58:14 +0200

    Disable the 'Show structogram' button if the programming language isn't Karol

--- test/spec/views/editor_toolbar.js
+++ test/spec/views/editor_toolbar.js
@@ -2,18 +2,18 @@ describe("Editor Toolbar", function () {
   var project, editorToolbar
 
   beforeEach(function () {
-    project = new Karel.Models.Project({ code: 'trolololo', language: 'javascript' })
+    project = new Karel.Models.Project({ code: 'trolololo', language: 'karol' })
     editorToolbar = new Karel.Views.EditorToolbar({ model: project })
     editorToolbar.render()
   })
 
   it("should have the language of the project preselected", function () {
-    expect(editorToolbar.$('.language-selection').val()).toBe('javascript')
+    expect(editorToolbar.$('.language-selection').val()).toBe('karol')
   })
 
   it("should change the language of the project when the user selects a language", function () {
-    editorToolbar.$('.language-selection').val('karol').trigger('change')
-    expect(project.get('language')).toBe('karol')
+    editorToolbar.$('.language-selection').val('javascript').trigger('change')
+    expect(project.get('language')).toBe('javascript')
   })
 
   it("should trigger 'show-structogram' if the user clicks this button", function () {
@@ -21,5 +21,12 @@ describe("Editor Toolbar", function () {
     editorToolbar.bind('show-structogram', showStructogramSpy)
     editorToolbar.$('.show-structogram-button').click()
     expect(showStructogramSpy).toHaveBeenCalled()
+  })
+
+  it("should disable the show-structugram-button if the language is javascript", function () {
+    var showStructogramButton = editorToolbar.$('.show-structogram-button')
+    expect(showStructogramButton.prop('disabled')).toBe(false)
+    project.set({ language: 'javascript' })
+    expect(showStructogramButton.prop('disabled')).toBe(true)
   })
 })
